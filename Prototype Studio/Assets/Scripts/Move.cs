9acc39724c88b0b22edb1f9fb29d9412f1fa4f31
cgbNYU <cgb316@nyu.edu>
2019-09-04 17:25:52 -0400

    added generic restart function will restart whatever the current scene is

---
+++ Prototype Studio/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Generic movement class that can move a character around either in 3D or 2D space
+//2D space can be a side scroller or top down
+public class Move : MonoBehaviour
+{
+    
+    //Public
+    public bool isSideScroll;
+    
+    //Private
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
