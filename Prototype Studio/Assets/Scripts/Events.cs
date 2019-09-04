b3d01391cce89c4646f7a8c46df7250690530227
cgbNYU <cgb316@nyu.edu>
2019-09-04 17:11:24 -0400

    Added dotween added event manager and stub class for events

---
+++ Prototype Studio/Assets/Scripts/Events.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class used to define events for the eventmanager
+public class Events : MonoBehaviour
+{
+    
+}
