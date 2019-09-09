09f2511eff09408585b5ba433f5585ac062cb9cb
cgbNYU <cgb316@nyu.edu>
2019-09-09 18:43:12 -0400

    completed triangle and square movement added assets for all shapes both player and npc

--- Prototype Studio/Assets/Scripts/ShapeMovement.cs
+++ Prototype Studio/Assets/Scripts/ShapeMovement.cs
@@ -10,25 +10,47 @@ public class ShapeMovement : MonoBehaviour
     public int PlayerNum;
     public float CircleSpeed;
     public float CircleFriction;
+    public float CircleDrag;
+    public float SquareSpeed;
+    public float SquareFriction;
+    public float SquareDrag;
+    public float SquareTime;
+    public float TriangleSpeed;
+    public float TriangleRotateSpeed;
+    public float TriangleFriction;
+    public float TriangleDrag;
     
     //Private
     private Rewired.Player rewiredPlayer;
     private Vector3 moveVector;
+    public bool squarePressed;
+    private float squareTimer;
     
-    //Enumerator
-    enum PlayerShape
+    //Enumerators
+    public enum PlayerShape
     {
         Circle,
         Square,
         Triangle
     }
 
-    private PlayerShape shape;
+    public PlayerShape shape;
+
+    enum PlayerColor
+    {
+        Red,
+        Yellow,
+        Blue,
+        Green,
+        Purple,
+        Orange
+    }
+
+    private PlayerColor color;
     
     // Start is called before the first frame update
     void Start()
     {
-        shape = PlayerShape.Circle;
         rewiredPlayer = ReInput.players.GetPlayer(PlayerNum); //assing player number in inspector
     }
 
@@ -41,8 +63,21 @@ public class ShapeMovement : MonoBehaviour
                 CircleMove();
                 break;
             case PlayerShape.Square:
+                SquareMove();
                 break;
             case PlayerShape.Triangle:
+                TriangleMove();
+                break;
+        }
+
+        switch (color)
+        {
+            case PlayerColor.Red:
+                Red();
+                break;
+            case PlayerColor.Blue:
+                break;
+            case PlayerColor.Yellow:
                 break;
         }
     }
@@ -53,6 +88,64 @@ public class ShapeMovement : MonoBehaviour
         transform.position += moveVector * CircleSpeed * Time.deltaTime;
         Vector3 friction = -moveVector.normalized * CircleFriction * Time.deltaTime;
         moveVector += friction;
+        Vector3 drag = -moveVector * CircleDrag * Time.deltaTime;
+        moveVector += drag;
         Debug.Log("Move Vector = " + moveVector);
+    }
+
+    void SquareMove()
+    {
+        if (Mathf.Abs(rewiredPlayer.GetAxisRaw("Horizontal")) > 0 && !squarePressed)
+        {
+            moveVector += Vector3.right * rewiredPlayer.GetAxisRaw("Horizontal");
+            squarePressed = true;
+        }
+        else if (Mathf.Abs(rewiredPlayer.GetAxisRaw("Vertical")) > 0 && !squarePressed)
+        {
+            moveVector += Vector3.up * rewiredPlayer.GetAxisRaw("Vertical");
+            squarePressed = true;
+        }
+
+        transform.position += moveVector * SquareSpeed * Time.deltaTime;
+        Vector3 friction = -moveVector.normalized * SquareFriction * Time.deltaTime;
+        moveVector += friction;
+        Vector3 drag = -moveVector * SquareDrag * Time.deltaTime;
+        moveVector += drag;
+
+        if (squarePressed)
+        {
+            squareTimer -= Time.deltaTime;
+            if (squareTimer <= 0)
+            {
+                squarePressed = false;
+                squareTimer = SquareTime;
+            }
+        }
+    }
+
+    void TriangleMove()
+    {
+        moveVector += transform.up * rewiredPlayer.GetAxisRaw("Vertical");
+        transform.position += moveVector * TriangleSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.back * rewiredPlayer.GetAxisRaw("Horizontal") * TriangleRotateSpeed);
+        Vector3 friction = -moveVector.normalized * TriangleFriction * Time.deltaTime;
+        moveVector += friction;
+        Vector3 drag = -moveVector * TriangleDrag * Time.deltaTime;
+        moveVector += drag;
+    }
+
+    void Red()
+    {
+        
+    }
+
+    public void ShapeChange()
+    {
+        
+    }
+
+    public void ColorChange()
+    {
+        
     }
 }
