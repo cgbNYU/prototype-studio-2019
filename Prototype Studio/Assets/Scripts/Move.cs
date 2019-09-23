7d6281ea85a6f54242630463200046f03da721fd
cgbNYU <cgb316@nyu.edu>
2019-09-23 18:09:21 -0400

    started project 3

--- Prototype Studio/Assets/Scripts/Move.cs
+++ Prototype Studio/Assets/Scripts/Move.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 //Generic movement class that can move a character around either in 3D or 2D space
 //2D space can be a side scroller or top down
@@ -9,19 +10,22 @@ public class Move : MonoBehaviour
     
     //Public
     public float Speed;
+    public int PlayerNum;
     
     //Private
+    private Rewired.Player _rewiredPlayer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rewiredPlayer = ReInput.players.GetPlayer(PlayerNum); //initialize rewired
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveVector = new Vector3(_rewiredPlayer.GetAxis("Horizontal"), _rewiredPlayer.GetAxis("Vertical"), 0);
+        transform.position += moveVector * Speed * Time.deltaTime;
     }
     
     
