df50b775b1a28d77cc1eb67da08b28be77f7e10d
meisteronly <chapinbr@gmail.com>
2019-09-25 14:24:54 -0400

    3 doine

---
+++ Prototype Studio/Assets/Scripts/FollowSausage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FollowSausage : MonoBehaviour
+{
+    
+    //Public
+    public float Speed;
+    
+    //private
+    private GameObject _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _target = GameObject.FindWithTag("Sausage");
+        if (_target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, Speed * Time.deltaTime);
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sausage"))
+        {
+            Debug.Log("Sausage Eat");
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Prototype Studio/Assets/Scripts/GuardSpot.cs
+++ Prototype Studio/Assets/Scripts/GuardSpot.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class GuardSpot : MonoBehaviour
 {
+    private Transform playerTransform;
+    private Transform dogTransform;
+    //Public
+    public float ViewDist;
+    public float ViewAngle;
+    public float Speed;
+    public float DeathSpeed;
+    
+    //Private
+    private bool _isAttacking;
+    private bool _isLooking;
+    private Rigidbody2D _rb;
+    public Collider2D _viewCone;
+
+    private Vector3 _target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _isAttacking = false;
+        _isLooking = true;
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        dogTransform = GameObject.FindWithTag("Dog").transform;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (_isAttacking)
+        {
+            Attack();
+        }
+    }
+
+    public void ViewCheck(Transform target)
+    {
+        if (_isLooking)
+        {
+            _isAttacking = true;
+            _target = target.position;
+        }
+    }
+
+    private void Attack()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, Speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isAttacking)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Dog"))
+            {
+                Debug.Log("Dead Player");
+                Vector3 forceVector = other.transform.position - transform.position;
+                other.GetComponent<Rigidbody2D>().AddForce(forceVector.normalized * DeathSpeed);
+                other.GetComponent<Rigidbody2D>().AddTorque(1000);
+            }
+        }
+        else if (other.CompareTag("Dog"))
+        {
+            Debug.Log("GuardDie");
+            Vector3 forceVector = transform.position - other.transform.position;
+            _rb.AddForce(forceVector.normalized * DeathSpeed);
+            _rb.AddTorque(1000);
+            _isAttacking = false;
+            _isLooking = false;
+        }
     }
 }
