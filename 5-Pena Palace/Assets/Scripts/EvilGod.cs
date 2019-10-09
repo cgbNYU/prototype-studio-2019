16155ddeec1a88f290d369cd37aff9c0117fee16
meisteronly <chapinbr@gmail.com>
2019-10-09 15:29:23 -0400

    complete

--- 5-Pena Palace/Assets/Scripts/EvilGod.cs
+++ 5-Pena Palace/Assets/Scripts/EvilGod.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EvilGod : MonoBehaviour
 {
     //Public
+    public GameObject MouseFollow;
+    public Lightning LightningScript;
+    public float LightningForce;
     
     //Private
     private Camera _cam;
@@ -18,9 +21,18 @@ public class EvilGod : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousPos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        mousPos.z = 0;
+        MouseFollow.transform.position = mousPos;
         if (Input.GetMouseButtonDown(0))
         {
-            
+            RaycastHit2D ray = Physics2D.Raycast(MouseFollow.transform.position, Vector2.down, 100000f);
+            if (ray.collider.CompareTag("Brick"))
+            {
+                Vector2 impulseDir = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+                LightningScript.ZapTarget(ray.collider.gameObject);
+                ray.collider.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(LightningForce * Vector2.down, ForceMode2D.Impulse);
+            }
         }
     }
 }
