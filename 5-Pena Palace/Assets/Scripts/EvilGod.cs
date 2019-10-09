b14d7183d4d6e8677f4b7a9a3768e80f86504db6
cgbNYU <cgb316@nyu.edu>
2019-10-09 12:52:42 -0400

    added pena palace project

---
+++ 5-Pena Palace/Assets/Scripts/EvilGod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilGod : MonoBehaviour
+{
+    //Public
+    
+    //Private
+    private Camera _cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+}
