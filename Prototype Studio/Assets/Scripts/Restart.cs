7db35efeec7379e50b7832d7efccdb641c075bb8
cgbNYU <cgb316@nyu.edu>
2019-10-28 18:46:34 -0400

    mostly done

--- Prototype Studio/Assets/Scripts/Restart.cs
+++ Prototype Studio/Assets/Scripts/Restart.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 //hit r to reload the scene
 public class Restart : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
---
+++ Prototype Studio/Assets/Scripts/SpawnObstacles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour
+{
+
+    public GameObject[] Obstacles;
+
+    private float _timer;
+
+    public float SpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = SpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            Spawn();
+            _timer = SpawnTime;
+        }
+    }
+
+    private void Spawn()
+    {
+        int random = Random.Range(0, Obstacles.Length - 1);
+        Instantiate(Obstacles[random], transform.position, transform.rotation);
+    }
+}
