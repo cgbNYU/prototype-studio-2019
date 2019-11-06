84012111b6c0108710622c089f2dcb644b384645
meisteronly <chapinbr@gmail.com>
2019-11-06 15:29:45 -0500

    Alien done

---
+++ GiftPrototype/Assets/Scripts/Alien.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Alien : MonoBehaviour
+{
+
+    private GameObject _player;
+
+    private bool _isRunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("FPSController");
+        EventManager.Instance.AddHandler<Events.GunShot>(OnGunShot);
+        _isRunning = false;
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.Instance.RemoveHandler<Events.GunShot>(OnGunShot);
+    }
+
+    public void OnGunShot(Events.GunShot evt)
+    {
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, _player.transform.position, out hit))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                Vector3 dist = _player.transform.position - transform.position;
+                transform.position += dist.normalized * 1 * Time.deltaTime;
+                if (dist.magnitude <= 15)
+                {
+                    _isRunning = true;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, _player.transform.position, out hit))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                Vector3 dist = _player.transform.position - transform.position;
+                if (!_isRunning)
+                {
+                    transform.position += dist.normalized * 1 * Time.deltaTime;
+                }
+                else
+                {
+                    transform.position += dist.normalized * 8 * Time.deltaTime;
+                }
+                
+                if (dist.magnitude <= 1)
+                {
+                    Debug.Log("KillPlayer");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+    }
+}
---
+++ GiftPrototype/Assets/Scripts/Shoot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Light Flash;
+
+    private bool _isShot;
+
+    private float timer;
+
+    private AudioSource source;
+
+    public AudioClip shotSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isShot = false;
+        Flash.enabled = false;
+        timer = .15f;
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !_isShot)
+        {
+            EventManager.Instance.Fire(new Events.GunShot());
+            source.PlayOneShot(shotSound);
+            _isShot = true;
+            Flash.enabled = true;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 15))
+            {
+                if (hit.transform.CompareTag("Alien"))
+                {
+                    Debug.Log("Kill Alien");
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+
+        if (_isShot)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = .15f;
+                Flash.enabled = false;
+                _isShot = false;
+            }
+        }
+    }
+}
